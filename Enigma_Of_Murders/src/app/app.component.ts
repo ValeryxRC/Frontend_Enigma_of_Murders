a0916b97aa5bc4d3f6fa2b353de4be91b791b834
Lau <lauraortizgarrido04@gmail.com>
2023-10-10 18:35:50 +0200

    Laura: cambios front

--- Enigma_Of_Murders/src/app/app.component.ts
+++ Enigma_Of_Murders/src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Enigma Of Murders';
+  ceosInfo = [
+
+    {
+      nombre: "Laura Ortiz",
+      imagen: "assets/img/lauraAvatar.png",
+      cargo: "CEO",
+      especialidad: "Front-End"
+    },
+
+    {
+      nombre: "Isabel Arellano",
+      imagen: "assets/img/valeryAvatar.png",
+      cargo: "CEO",
+      especialidad: "Back-End"
+    },
+
+    {
+      nombre: "Valery Rodriguez",
+      imagen: "assets/img/isabelAvatar.png",
+      cargo: "CEO",
+      especialidad: "Back-End"
+    },
+
+    {
+      nombre: "Aakriti",
+      imagen: "assets/img/aakritiAvatar.png",
+      cargo: "CEO",
+      especialidad: "Front-End"
+    }
+  ];
+
+  socialMedias = [
+    {
+      red: "IG",
+      link: [{text:"@studio_vila", url:"https://www.instagram.com/"}]
+    },
+    {
+      red: "Gmail",
+      link: [{text:"studiovila@gmail.com", url:"https://www.google.com/intl/es/gmail/about/"}]
+    }
+  ]
+  copyright = '@copyright Studio VILA';
 }
